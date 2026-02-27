f0acf053db85fc58d1b540fd9f97874db3ef9bb4
sarankasa04-blip <sarankasa04@gmail.com>
2026-02-27 10:39:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Restaurant backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
